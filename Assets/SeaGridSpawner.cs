304ffc198a7de97e0c34ece74ddeb4c20a978562
Wessel Frijters <wessel.frijters@gmail.com>
2018-11-09 12:09:01 +0100

    Fixed Sea Mesh

---
+++ Assets/SeaGridSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeaGridSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private int m_XSize, m_YSize;
+
+    [SerializeField]
+    private GameObject m_SeaObject;
+
+    [SerializeField]
+    private int m_SeaXSize, m_SeaYSize;
+
+    [SerializeField]
+    private float m_Resolution;
+
+	// Use this for initialization
+	void Start () {
+		for(int x = 0; x < m_XSize; x++)
+        {
+            for(int y = 0; y < m_YSize; y++)
+            {
+                GameObject sea = Instantiate(m_SeaObject, new Vector3(x * m_SeaXSize * m_Resolution, 0, y * m_SeaYSize * m_Resolution), Quaternion.identity);
+                sea.transform.parent = this.transform;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Shaders/Water/SeaMeshImproved.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class SeaMeshImproved : MonoBehaviour
+{
+
+    [SerializeField]
+    private int m_XSize, m_YSize;
+
+    [SerializeField]
+    private float m_Resolution;
+
+    private Vector3[] m_Vertices;
+
+    private Mesh m_Mesh;
+
+    private void Awake()
+    {
+        Generate();
+    }
+
+    private void Generate()
+    {
+        GetComponent<MeshFilter>().mesh = m_Mesh = new Mesh();
+        m_Mesh.name = "Procedural Grid";
+
+        List<Vector3> vertices = new List<Vector3>();
+        for (int y = 0; y < m_YSize; y++)
+        {
+            for (int x = 0; x < m_XSize; x++)
+            {
+                vertices.Add(new Vector3(x * m_Resolution, 0, y * m_Resolution));
+            }
+        }
+
+        m_Vertices = vertices.ToArray();
+        m_Mesh.vertices = m_Vertices;
+
+        List<int> triangles = new List<int>();
+        int triangleQuads = (m_XSize - 1) * (m_YSize - 1); //Triangle Quad consist of two triangles.
+        Debug.Log(triangleQuads);
+
+        int xQuad = 0;
+        for (int i = 0; i < triangleQuads; i++)
+        {
+            xQuad++;
+            //3 2
+            //1 0
+            
+            //Skip Edge
+            if(xQuad == m_XSize) { xQuad = 0; triangleQuads++; continue; }
+
+            triangles.Add(m_XSize + 1 + i);
+            triangles.Add(i+1); //1
+            triangles.Add(i); //0
+
+            triangles.Add(i); //0
+            triangles.Add(m_XSize + i); //2
+            triangles.Add(m_XSize + 1 + i); //3
+        }
+
+
+        m_Mesh.triangles = triangles.ToArray();
+        m_Mesh.RecalculateNormals();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(m_Vertices != null)
+        {
+            foreach(Vector3 vert in m_Vertices)
+            {
+                Gizmos.DrawSphere(vert, 0.1f);
+            }
+        }
+    }
+
+}
